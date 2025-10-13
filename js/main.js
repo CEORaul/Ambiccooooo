3935e61b3f0f7dc5b3ef8e27e96c9ec80417f658
Raul Santos <raulssantos167@gmail.com>
2025-10-13 11:58:38 -0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Ambicco rodando com sucesso!");
+// Popup de sucesso
+const form = document.getElementById('contactForm');
+const popup = document.getElementById('popup');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  popup.style.display = 'block';
+  setTimeout(() => popup.style.display = 'none', 3000);
+  form.reset();
+});
